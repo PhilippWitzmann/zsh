7be7931809760a551ed19fef6b6eba98fbc0f9eb
Philipp Witzmann <witzmann@contsult.com>
2018-07-02 13:52:02 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Zsh Config by Philipp Witzmann
+
+## Installation
+ * Clone this Repo (git clone git@github.com:PhilippWitzmann/zsh.git)
+ * Install it (currently it is only written for Mac!): cd path/to/repo && ./install.sh
+ * Change the terminal font to what gets output by install script
+
+## Contribute
+ * Because this is my personal setting, i don't need contributions. However, if you have something amazing that you absolutely need to share, or you found a bug, please open an issue and i'll get in touch with you.
---
+++ install.sh
@@ -0,0 +1,15 @@
+# Installation Script for zsh config
+
+echo 'Installing zsh';
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo 'Installing fonts'
+cp SourceCodePro+Powerline+Awesome+Regular.ttf /Library/Fonts/
+
+echo 'Creating symlink for config file';
+
+ln zshrc ~/.zshrc;
+
+echo 'Done. Don\'t forget to change terminal font to SourceCodePro+Powerline+Awesome+Regular. Happy bashing!'
+
+echo 'Change Terminal Font on Mac: http://osxdaily.com/2011/09/02/use-any-font-in-the-mac-os-x-lion-terminal'
