dd3f57a75c7cacfdfe524153a2808a402abdfede
Philipp Witzmann <pwitzmann2@gmail.com>
2021-03-04 11:02:44 +0100

    Update install_linux.sh

--- install_linux.sh
+++ install_linux.sh
@@ -233,7 +233,7 @@ chmod +x prettyping
 sudo mv prettyping /usr/local/bin/
 
 headline 'Install helm'
-curl -LO https://git.io/get_helm.sh
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
 rm get_helm.sh
